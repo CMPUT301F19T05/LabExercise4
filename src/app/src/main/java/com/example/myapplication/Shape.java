da6833ea845ad7bc4fff8c1aa02df75e7c29288b
jonong1 <jzong@ualberta.ca>
2019-10-02 17:49:39 -0600

    add Star

--- src/app/src/main/java/com/example/myapplication/Shape.java
+++ src/app/src/main/java/com/example/myapplication/Shape.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 public abstract class Shape {
+    public String color;
     public int x;
     public int y;
 }
--- src/app/src/main/java/com/example/myapplication/Star.java
+++ src/app/src/main/java/com/example/myapplication/Star.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
 
 public class Star extends Shape {
+
 }
